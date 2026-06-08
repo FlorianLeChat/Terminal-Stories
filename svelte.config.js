7c73dc04130614990b412fe4bfbad3c8025f9293
Florian Trayon <git@florian-trayon.fr>
2026-06-08 19:13:02 +0200

    feat(lint): add ESLint, Husky, Prettier, and other linting tools to align with other projects

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter()
-	}
+    kit: {
+        typescript: {
+            config: ( config ) =>
+            {
+                config.include.push( "../*.config.*" );
+                return config;
+            }
+        },
+        adapter: adapter(),
+        paths: {
+            // @ts-expect-error Needed for GitLab Pages generation.
+            base: process.argv.includes( "dev" ) ? "" : process.env.BASE_PATH
+        }
+    }
 };
 
 export default config;
